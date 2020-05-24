7bb147dfdcc50cab3fe713214cad3358377c3f55
duyanh1992 <alyssachia1992@gmail.com>
2020-05-24 11:34:15 +0700

    create list cart action

--- src/components/MainSite/Cart.js
+++ src/components/MainSite/Cart.js
@@ -2,41 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Title from '../../theme/styles/Title';
 import color from '../../theme/color';
-
-
-const CartItem = styled.div`
-    border: 1px dotted ${color.gray};
-
-    .prd-img {
-        img {
-            width: 100%;
-        }
-    }
-
-    .cart-item-info {
-        list-style: none;
-
-        li {
-            padding: 10px 0;
-
-            label {
-                color:${color.black};
-                font-weight: bold;
-                width: 150px;
-                text-transform: capitalize;
-            }
-
-            input[type="number"] {
-                width: 10%;
-            }
-
-            .item-price, .item-total {
-                color: ${color.brown};
-	            font-weight: bold;
-            }
-        }
-    }
-`;
+import CartItem from './CartItem';
 
 const CartFooter = styled.div`
     .cart-total {
@@ -46,7 +12,23 @@ const CartFooter = styled.div`
 `;
 
 export default class Cart extends Component {
+    componentDidMount() {
+        this.props.getUserCartInfo(this.props.userId);
+    }
+
+    renderCartItems() {
+        const { cart, userId } = this.props;
+
+        if(cart && cart.userId === userId && cart.cartItems.length > 0) {
+            return cart.cartItems.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />);
+        }
+
+        return <h3>There is no item in your cart</h3>;
+    }
+
     render() {
+        const { cart } = this.props;
+        
         return (
             /* Cart */
             <div className="user-cart col-md-9">
@@ -54,100 +36,12 @@ export default class Cart extends Component {
                     <Title className="title">your cart</Title>
                 </div>
                 <div className="cart-list">
-                    <CartItem className="cart-item mb-4">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-3">
-                                    <figure className="prd-img">
-                                        <img src="images/prd05.jpg" alt="" />
-                                    </figure>	
-                                </div>
-                                <ul className="col-md-9 cart-item-info pl-5">
-                                    <li>
-                                        <label>product name:</label>
-                                        <span className="item-name">Samsung Galaxy Note Trắng</span>
-                                    </li>
-                                    <li>
-                                        <label>price:</label>
-                                        <span className="item-price">6.800.000 VNĐ</span>
-                                    </li>
-                                    <li>
-                                        <label>Amount:</label>
-                                        <input className="item-amount" type="number" value={1}/>
-                                    </li>
-                                    <li>
-                                        <label>total:</label>
-                                        <span className="item-total">27.200.000 VNĐ</span>
-                                    </li>
-                                    <button type="button" className="btn btn-danger float-right">Remove this item</button>
-                                </ul>
-                            </div>
-                        </div>						
-                    </CartItem>
-                    <CartItem className="cart-item mb-4">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-3">
-                                    <figure className="prd-img">
-                                    <img src="images/prd05.jpg" alt="" />
-                                    </figure>	
-                                </div>
-                                <ul className="col-md-9 cart-item-info pl-5">
-                                    <li>
-                                        <label>product name:</label>
-                                        <span className="item-name">Samsung Galaxy Note Trắng</span>
-                                    </li>
-                                    <li>
-                                        <label>price:</label>
-                                        <span className="item-price">6.800.000 VNĐ</span>
-                                    </li>
-                                    <li>
-                                        <label>Amount:</label>
-                                        <input className="item-amount" type="number" value={1} />
-                                    </li>
-                                    <li>
-                                        <label>total:</label>
-                                        <span className="item-total">27.200.000 VNĐ</span>
-                                    </li>
-                                    <button type="button" className="btn btn-danger float-right">Remove this item</button>
-                                </ul>
-                            </div>
-                        </div>						
-                    </CartItem>
-                    <CartItem className="cart-item mb-4">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-3">
-                                    <figure className="prd-img">
-                                        <img src="images/prd05.jpg" alt="" />
-                                    </figure>	
-                                </div>
-                                <ul className="col-md-9 cart-item-info pl-5">
-                                    <li>
-                                        <label>product name:</label>
-                                        <span className="item-name">Samsung Galaxy Note Trắng</span>
-                                    </li>
-                                    <li>
-                                        <label>price:</label>
-                                        <span className="item-price">6.800.000 VNĐ</span>
-                                    </li>
-                                    <li>
-                                        <label>Amount:</label>
-                                        <input className="item-amount" type="number" value={1} />
-                                    </li>
-                                    <li>
-                                        <label>total:</label>
-                                        <span className="item-total">27.200.000 VNĐ</span>
-                                    </li>
-                                    <button type="button" className="btn btn-danger float-right">Remove this item</button>
-                                </ul>
-                            </div>
-                        </div>						
-                    </CartItem>
+                    {/* <CartItem /> */}
+                    {this.renderCartItems()}
 
                     <CartFooter className="cart-footer">
                         <p>
-                            Tổng giá trị giỏ hàng là: <span className="cart-total">108.800.000 VNĐ</span>
+                            Tổng giá trị giỏ hàng là: <span className="cart-total">{cart.cartTotal} VNĐ</span>
                         </p>
                     </CartFooter>
                 </div>
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,11 +1,26 @@
 import * as mainSiteTypes from './../constants/mainsite';
 
-const initialState = [];
+const initialState = {
+    userId: null,
+    cartItems: [],
+    cartTotal: null,
+}
+
 
 const cart = (state = initialState, action) => {
     switch(action.type) {
         case mainSiteTypes.ADD_PRODUCT_TO_CART_SUCCESS: {
             const result = {...state};
+            return result;
+        }
+
+        case mainSiteTypes.GET_USER_CART_INFO_SUCCESS: {
+            const result = {
+                ...state,
+                userId: action.userId,
+                cartItems: action.data,
+                cartTotal: action.totalPrice,
+            };
 
             return result;
         }
